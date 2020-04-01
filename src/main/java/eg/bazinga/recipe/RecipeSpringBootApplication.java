7b0edc7f859710324c07866d9e4d86e03297586f
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-01 18:35:29 +0200

    First Commit

---
+++ src/main/java/eg/bazinga/recipe/RecipeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package eg.bazinga.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eg/bazinga/recipe/RecipeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.bazinga.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
